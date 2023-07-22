7b0d0b2c69a6df17cd7ab5055d28b58f893117c5
Peter Hua <37195345+PeterSHua@users.noreply.github.com>
2023-07-22 08:08:53 -0700

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
 import express from "express";
-const app = express();
 import cors from "cors";
+
 import binRoutes from "./controllers/binRoutes.js";
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+import {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+} from "./middleware/middleware.js";
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("build"));
 
-// ROUTES
 app.use("/bin", binRoutes);
 app.use(unknownEndpoint);
 app.use(errorHandler);
--- src/controllers/binRoutes.js
+++ src/controllers/binRoutes.js
@@ -5,43 +5,52 @@ import { v4 as uuidv4 } from "uuid";
 
 const binRoutes = Router();
 
-binRoutes.get("/", async (req, res) => {
+// Get all bins
+binRoutes.get("/", async (_req, res) => {
   const sql = "SELECT * FROM bin";
   const results = await pool.query(sql);
+
   res.send(results.rows).end();
 });
 
-binRoutes.get("/new", async (req, res) => {
+// New bin
+binRoutes.get("/new", async (_req, res) => {
   const uuid = uuidv4();
   const sql = "INSERT INTO bin (endpoint) VALUES ($1)";
   await pool.query(sql, [uuid]);
+
   res.send(uuid).end();
 });
 
+// Get payloads from bin
 binRoutes.get("/:uuid", async (req, res) => {
   const uuid = req.params.uuid;
-  const sql = "SELECT id FROM bin WHERE endpoint=$1";
-  const result = await pool.query(sql, [uuid]);
+  const getBinId = "SELECT id FROM bin WHERE endpoint=$1";
+  const result = await pool.query(getBinId, [uuid]);
   const id = result.rows[0].id;
-  const sql2 =
+
+  const getPayloads =
     "SELECT http_request, http_timestamp FROM payload WHERE bin_id=$1";
-  const payloadRequests = await pool.query(sql2, [String(id)]);
+  const payloadRequests = await pool.query(getPayloads, [String(id)]);
   res.send(payloadRequests.rows).end();
 });
 
-binRoutes.delete("/:uuid", async (req, res) => {
+// Delete bin
+binRoutes.delete("/:uuid", async (_req, _res) => {
   //
 });
 
+// Deliver payload to bin
 binRoutes.post("/:uuid", async (req, res) => {
   const uuid = req.params.uuid;
-  const sql = "SELECT id FROM bin WHERE endpoint=$1";
-  const result = await pool.query(sql, [uuid]);
+  const getBinId = "SELECT id FROM bin WHERE endpoint=$1";
+  const result = await pool.query(getBinId, [uuid]);
   const id = result.rows[0].id;
+
   const timeStamp = new Date();
   req = jsonStringifySafe(req);
-  const sql2 = `INSERT INTO payload (http_request, http_timestamp, bin_id) VALUES ($1, $2, $3) RETURNING http_request`;
-  await pool.query(sql2, [req, timeStamp, id]);
+  const insertPayload = `INSERT INTO payload (http_request, http_timestamp, bin_id) VALUES ($1, $2, $3) RETURNING http_request`;
+  await pool.query(insertPayload, [req, timeStamp, id]);
 
   res.status(200).end();
 });
